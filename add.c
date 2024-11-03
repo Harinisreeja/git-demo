af3b4621decf22631515c2552c8086b201726c13
HariniSreejaSatyavarapu <sreejasatyavarapu@HariniSreeja-23032010.>
2024-11-03 11:41:09 +0000

    Implemented loops in addition

--- add.c
+++ add.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
-int main () {
-	int a,b;
-	int sum;
-	printf("Enter first integer:");
-	scanf("%d",&a);
-	printf("Enter second integer:");
-	scanf("%d",&b);
-        sum =a+b;
-	printf("The sum of %d and %d is %d\n",a,b,sum);
-	return 0;
+
+int add(int i,int j)
+{
+        return (i+j);
 }
+int main () {
+        int a,b;
+        int sum;
+        char confirm ='n';
+        do {
+                printf("Enter first integer:");
+                scanf("%d",&a);
+                printf("Enter second integer:");
+                scanf("%d",&b);
+                sum= add(a,b);
+                printf("the sum of %d and %d is %d\n",a,b,sum);
+                printf("Do you have more sums for me [y/N]?");
+                scanf("%c",&confirm);
+        }
+        while(confirm =='y' || confirm =='Y');
+        return 0;
+}
+
