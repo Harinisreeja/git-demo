87de66a67e7168608b7fcdaf55325451fd8bcd0f
HariniSreejaSatyavarapu <sreejasatyavarapu@HariniSreeja-23032010.>
2024-11-03 08:46:24 +0000

    Added user inputs;also added .gitignore

--- add.c
+++ add.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
 int main () {
-	int a=3,b=2;
-	int sum= a+b;
-	printf("the sum od %d and %d is %d\n",a,b,sum);
+	int a,b;
+	int sum;
+	printf("Enter first integer:");
+	scanf("%d",&a);
+	printf("Enter second integer:");
+	scanf("%d",&b);
+        sum =a+b;
+	printf("The sum of %d and %d is %d\n",a,b,sum);
 	return 0;
 }
